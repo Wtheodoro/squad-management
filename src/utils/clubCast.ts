1e079330246ebccc2b65d01c6af15440526f5a80
WTheodoro <theodoro.walison@gmail.com>
2021-04-06 22:28:27 -0300

    athletes

--- src/utils/clubCast.ts
+++ src/utils/clubCast.ts
@@ -1,57 +1,788 @@
 export const club_cast = [
+    // Athletico paranaense
     {
-        name: 'Aderbar Melo dos Santos Neto',
-        nacionality: 'Brazil',
+        name: 'Santos',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Goleiro'
     },
     {
         name: 'Lucho González',
         nacionality: 'Argentina',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Meio-campista'
     },
     {
         name: 'Márcio Azevedo',
-        nacionality: 'Brazil',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Meio-campista'
     },
     {
         name: 'Denner Fernando Melz',
-        nacionality: 'Brazil',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Defensor'
     },
     {
-        name: 'Nikão - Maycon Vinícius Ferreira da Cruz',
-        nacionality: 'Brazil',
+        name: 'Nikão',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Atacante'
     },
     {
         name: 'Khellven Douglas Silva',
-        nacionality: 'Brazil',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Defensor'
     },
     {
         name: 'Jaime Alvarado',
         nacionality: 'Columbia',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Defensor'
     },
     {
         name: 'Abner Vinicius da Silva Santos',
-        nacionality: 'Brazil',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Defensor'
     },
     {
         name: 'Léo Cittadini',
-        nacionality: 'Brazil',
+        nacionality: 'Brasil',
         age: 30,
-        club: 'Athletico Paranaense'
+        club: 'Athletico Paranaense',
+        position: 'Defensor'
+
     },
-    
+
+    // Atlético goianiense
+    {
+        name: 'Fernando Miguel',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Goleiro'
+
+    },
+    {
+        name: 'Arnaldo',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Defensor'
+    },
+    {
+        name: 'Dudu',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Defensor'
+    },
+    {
+        name: 'Natanael',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Defensor'
+    },
+    {
+        name: 'Rithely',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Everton Felipe',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Roberson',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Atacante'
+    },
+    {
+        name: 'André Luiz',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Atacante'
+    },
+    {
+        name: 'Arthur Gomes',
+        nacionality: 'Brasil',
+        age: 29,
+        club: 'Atlético Goianiense',
+        position: 'Atacante'
+    },
+
+    // Atlético Mineiro
+    {
+        name: 'Kafunga',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Veludo',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Mazurkiewicz',
+        nacionality: 'Uruguai',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Carlos Brant',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Nariz',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Procópio Cardoso',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Grapete',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Vantuir',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Cléber',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro'
+    },
+    {
+        name: 'Léo Silva',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Réver',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Nelinho',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Zezé Procópio',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Bigode',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro'
+    },
+    {
+        name: 'Dedê',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Belleti',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Defensor'
+    },
+    {
+        name: 'Toninho Cerezo',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Ronaldinho Gaúcho',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Dadá Maravilha',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Atacante'
+    },
+    {
+        name: 'Guará',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Atacante'
+    },
+    {
+        name: 'Orlando Pingo de Ouro',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Atacante'
+    },
+    {
+        name: 'Diego Tardelli',
+        nacionality: 'Brasil',
+        age: 28,
+        club: 'Atlético Mineiro',
+        position: 'Atacante'
+    },
+    // Bahia
+    {
+        name: 'Douglas',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: 'Defensor'
+    },
+    {
+        name: 'Nino Paraíba',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Anderson Martins',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: 'Defensor'
+    },
+    {
+        name: 'Douglas Borel',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia'
+    },
+    {
+        name: 'Germán Conti',
+        nacionality: 'Argentina',
+        age: 27,
+        club: 'Bahia',
+        position: 'Defensor'
+    },
+    {
+        name: 'Índio Ramirez',
+        nacionality: 'Colômbia',
+        age: 27,
+        club: 'Bahia',
+        position: 'Atacante'
+    },
+    {
+        name: 'João Pedro',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Lucas Fonseca',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: 'Defensor'
+    },
+    {
+        name: 'Juninho Capixaba',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Bahia',
+        position: ''
+    },
+
+    // Botafogo
+    {
+        name: 'Ricardo Cruz',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Fernando Álvez',
+        nacionality: 'Uruguai',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Gatito Fernández',
+        nacionality: 'Paraguai',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Jefferson',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo'
+    },
+    {
+        name: 'Manga',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Roberto Gomes Pedrosa',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'William Bacana',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Carli',
+        nacionality: 'Argentina',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Defensor'
+    },
+    {
+        name: 'Nilton Santos',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Defensor'
+    },
+    {
+        name: 'Oscar Basso',
+        nacionality: 'Argentina',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Defensor'
+    },
+    {
+        name: 'Carlos Alberto Santos',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Defensor'
+    },
+    {
+        name: 'Geninho',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Martim Silveira',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Nei Conceição',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Seedorf',
+        nacionality: 'Suriname/Holanda',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Túlio',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Valdo',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Abelardo de Lamare',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Dino da Costa',
+        nacionality: 'Itália',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Fischer',
+        nacionality: 'Argentina',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Flavio Ramos',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Jairzinho',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Leônidas da Silva',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Loco Abreu',
+        nacionality: 'Uruguai',
+        age: 26,
+        club: 'Botafogo'
+    },
+    {
+        name: 'Luís Menezes',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Mirandinha',
+        nacionality: 'Brasil',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+    {
+        name: 'Patesko',
+        nacionality: 'Polónia',
+        age: 26,
+        club: 'Botafogo',
+        position: 'Atacante'
+    },
+
+    // Ceará
+    {
+        name: 'João Ricardo',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Richard',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Messias',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Defensor'
+    },
+    {
+        name: 'Luiz Otávio',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Defensor'
+    },
+    {
+        name: 'Gabriel Lacerda',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Defensor'
+    },
+    {
+        name: 'William Oliveira',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Fabinho',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Oliveira',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Fernando Sobral',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Yony González',
+        nacionality: 'Colômbia',
+        age: 31,
+        club: 'Ceará',
+        position: 'Atacante'
+    },
+    {
+        name: 'Jael',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Atacante'
+    },
+    {
+        name: 'Steven Mendoza',
+        nacionality: 'Colômbia',
+        age: 31,
+        club: 'Ceará',
+        position: 'Atacante'
+    },
+    {
+        name: 'Felipe Vizeu',
+        nacionality: 'Brasil',
+        age: 31,
+        club: 'Ceará',
+        position: 'Atacante'
+    },
+
+    // Corinthians
+    {
+        name: 'Caíque França',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Cássio',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Goleiro'
+    },
+    {
+        name: 'Jemerson',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Gil',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Léo Santos',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Bruno Méndez',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Lucas Piton',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Fábio Santos',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Danilo Avelar',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Fagner',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Defensor'
+    },
+    {
+        name: 'Juan Cazares',
+        nacionality: 'Equador',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Rómulo Otero',
+        nacionality: 'Venezuela',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Luan',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Maheus Araújo',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Gabriel',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Ramiro',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Camacho',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Victor Cantillo',
+        nacionality: 'Colômbia',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Meio-campista'
+    },
+    {
+        name: 'Cauê',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Atacante'
+    },
+    {
+        name: 'Léo Natel',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Atacante'
+    },
+    {
+        name: 'Gustavo Silva',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Atacante'
+    },
+    {
+        name: 'Felipe Augusto',
+        nacionality: 'Brasil',
+        age: 27,
+        club: 'Corinthians',
+        position: 'Atacante'
+    },
 ]
