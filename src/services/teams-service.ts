86d1c79926f77b44ee412b0453fa8e62a53ff8b9
WTheodoro <theodoro.walison@gmail.com>
2021-04-08 10:45:44 -0300

    live

--- src/services/teams-service.ts
+++ src/services/teams-service.ts
@@ -5,8 +5,10 @@ const TeamService = {
     getTeams: (id: number) => api.get(`/times/${id}`,
     {headers: {
 
+        'Authorization': `Bearer live_2f970acbcaa028b7998045708f54a2`
+
         // Test autorizarion without limit but with only one return
-        'Authorization': `Bearer ${TEST_AUTH}`
+        // 'Authorization': `Bearer ${TEST_AUTH}`
 
         // Live authorization bring real result but only 100 requisitions/day
         // you should use your own key
